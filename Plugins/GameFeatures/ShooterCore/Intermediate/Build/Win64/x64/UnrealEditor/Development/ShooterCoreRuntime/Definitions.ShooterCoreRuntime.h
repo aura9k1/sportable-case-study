d24c7c5f9b90de3ebf20648ee969c2b4726367ca
aura9k1 <rjoshpaddy@gmail.com>
2025-07-14 16:13:48 +0200

    plugin builds
    push plugin builds for posterity

--- Plugins/GameFeatures/ShooterCore/Intermediate/Build/Win64/x64/UnrealEditor/Development/ShooterCoreRuntime/Definitions.ShooterCoreRuntime.h
+++ Plugins/GameFeatures/ShooterCore/Intermediate/Build/Win64/x64/UnrealEditor/Development/ShooterCoreRuntime/Definitions.ShooterCoreRuntime.h
@@ -1,4 +1,4 @@
-#include "C:/Users/Josh/Desktop/Sportable_CaseStudy_test/Sportable_CaseStudy/Intermediate/Build/Win64/x64/LyraEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "C:/Users/Josh/Documents/GitHub/sportable-case-study/Intermediate/Build/Win64/x64/LyraEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
